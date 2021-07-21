3558399b303beeaedff2fdc7d36fb0473becdebd
vytcka4 <vytautas.freelance@gmail.com>
2021-07-21 23:07:41 +0300

    commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const contactBtn = document.querySelector(".contact-me")
+const mainTextContainer = document.querySelector(".main-text-container")
+const contactBtnContainer = document.querySelector(".contact-button")
+const otherBtn =document.querySelector(".other-buttons");
+
+contactBtn.addEventListener("click", function(){
+  mainTextContainer.classList.add("hidden");
+  contactBtnContainer.classList.add("hidden");
+  otherBtn.classList.add("hidden");
+})
